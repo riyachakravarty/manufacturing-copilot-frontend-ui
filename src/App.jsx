4b3875952b1c37e1eff03e67256f5c93410b00e6
SANGEETA CHAKRAVARTY <principal@SANGEETAs-MacBook-Pro.local>
2026-02-23 01:01:05 +0530

    Keeping last plots when tab changes context file app file

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 import { AppProvider } from "./context/AppContext"; // Global context for file, mode, etc.
 import theme from "./theme"; // Custom MUI theme with dark blue and green shades
+import { PlotProvider } from "./context/PlotContext";
 
 import HomePage from "./pages/HomePage";
 import MainPage from "./pages/MainPage";
@@ -14,12 +15,14 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppProvider>
+      <PlotProvider>
         <Router>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/main" element={<MainPage />} />
           </Routes>
         </Router>
+        </PlotProvider>
       </AppProvider>
     </ThemeProvider>
   );
